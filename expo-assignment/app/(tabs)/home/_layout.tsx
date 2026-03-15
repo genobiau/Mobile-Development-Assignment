2ed4f15aca565eec50093e0ffc6b41ca09521fc2
Gen <uygyy001@unisa.edu.au>
2026-03-15 15:52:39 +1100

    explore.tsx done

--- expo-assignment/app/(tabs)/home/_layout.tsx
+++ expo-assignment/app/(tabs)/home/_layout.tsx
@@ -3,7 +3,10 @@ import { Stack } from 'expo-router';
 export default function HomeLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Home' }} />
+      <Stack.Screen
+        name="index"
+        options={{ title: 'NSW Traffic Incidents Search Homepage' }}
+      />
       <Stack.Screen name="search-incidents" options={{ headerShown: false }} />
       <Stack.Screen name="incidents" options={{ headerShown: false }} />
       <Stack.Screen name="[id]" options={{ headerShown: false }} />

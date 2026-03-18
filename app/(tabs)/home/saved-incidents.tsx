80994da50aa2ae9f8a2016d073034a2836d6cadd
Gen <uygyy001@unisa.edu.au>
2026-03-18 17:48:19 +1100

    update

--- app/(tabs)/home/saved-incidents.tsx
+++ app/(tabs)/home/saved-incidents.tsx
@@ -1,20 +1,35 @@
-import { Text, View, } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {useState} from 'react'
-
+import { useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+interface incident {
+  name: string;
+  streetName: string;
+}
 export default function SaveIncidentsPage() {
+  const [savedItems, setSavedItem] = useState<incident[]>([]);
 
-  const [savedItems, setSavedItem] = useState('');
-
-  const storeData = async () => {
+  const readData = async () => {
     try {
       let jsonValue = await AsyncStorage.getItem('');
-      jsonValue= jsonValue != null ? JSON.parse(jsonValue): null;
-      setSavedItem(jsonValue)
+      const incidents =
+        jsonValue != null ? (JSON.parse(jsonValue) as incident[]) : null;
+      if (incidents != null) {
+        setSavedItem(incidents);
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
+
+  useEffect(() => {
+    readData();
+  }, []);
   return (
     <View>
       <Text>Saved Incidents</Text>
+      {savedItems.map((incident) => {
+        return <Text key={incident.name}>{incident.name}</Text>;
+      })}
     </View>
   );
 }
